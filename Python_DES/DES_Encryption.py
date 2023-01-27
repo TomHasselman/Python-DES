19f15da900091918cde102a5888f13fce2e0626b
Tom Hasselman <thasselm@mix.wvu.edu>
2023-01-26 20:37:41 -0500

    got blocks working

--- Python_DES/DES_Encryption.py
+++ Python_DES/DES_Encryption.py
@@ -16,18 +16,25 @@ def string_to_binary(input_string):
         
     return return_string
     
-def make_block(input_string):
+def make_block(input_string, size):
     """Takes an string of binary data and breaks it into 64-bit chunks"""
-    return input_string[:64]
+    return input_string[:size]
         
-def make_list_of_blocks(input_string):
+def make_list_of_blocks(input_string, size):
     """Calls 'make_block()' multiple time to split the input_string into multiple 64-bit blocks. 
         Appends '0' to the end of the final block so that all blocks are uniform size. 
         Returns a list of 64-bit blocks."""
+    list_of_blocks = []
+    
+    for i in range(0, len(input_string), size):
+        list_of_blocks.append(input_string[i:i+size])
         
-    for i in range(len(input_string)):
-        print(i)
-
+    #check if the last block is the correct size. If not, append '0' to the end of the block    
+    if list_of_blocks[-1] != size:
+        list_of_blocks[-1] = list_of_blocks[-1].ljust(size, '0')
+        
+    return list_of_blocks
+        
 user_string = input("Enter an ASCII String: ")
 print(user_string)
 
@@ -37,7 +44,7 @@ print(stripped_user_string)
 
 binary_string = string_to_binary(stripped_user_string)
 
-print(f"This is the first 64-bit block from the input string: {make_block(binary_string)}")
+print(f"This is the first 64-bit block from the input string: {make_list_of_blocks(binary_string, 64)}")
 
 #decimal_value = ord(character)
 #binary_value = bin(decimal_value)[2:].zfill(8) #bin() prepends "0b" to the value, so slice it off. 
@@ -45,6 +52,3 @@ print(f"This is the first 64-bit block from the input string: {make_block(binary
 
 #print(f"This is the decimanl value of the character: {decimal_value}")
 #print(f"This is the binary value of the character: {binary_value}")
-
-
-    
